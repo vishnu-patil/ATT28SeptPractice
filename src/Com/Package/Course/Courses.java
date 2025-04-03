8f943d017669db1c55d0403406b08ddc30f35453
vishnu <patilvishnu9493@gmail.com>
2025-04-03 12:55:43 +0530

    Created New Class 04-03-2025 12:55:00

---
+++ src/Com/Package/Course/Courses.java
@@ -0,0 +1,14 @@
+package Com.Package.Course;
+
+public class Courses {
+
+	public void listOfCourses()
+	{
+		System.out.println("List of All Courses.");
+	}
+	
+	public void availableCourses()
+	{
+		System.out.println("List of Available Courses.");
+	}
+}
